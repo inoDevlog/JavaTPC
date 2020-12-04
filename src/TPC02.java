8e122b482fc7578feed55e5c2c3a49f8f749b9a5
choiinho <choiinho@knou.ac.kr>
2020-12-04 16:39:38 +0900

    init2

---
+++ src/TPC02.java
@@ -0,0 +1,23 @@
+
+public class TPC02 {
+	public static void main(String[] args) {
+		// 프로그래밍의 3대 요소 : 변수, 자료형(DataType), 할당(=)
+		// 1+1=2
+
+		int a, b, c; // 4byte. 자료혀은 크기와 종류가 정해져야 한다.
+		a = 1;
+		b = 1;
+		c = a + b;
+
+		System.out.println(c);
+		
+		float f; // float, double
+		f = 34.5f;
+		System.out.println(f);
+		
+		char d;
+		d = 'A';
+		System.out.println(d);
+	}
+
+}
