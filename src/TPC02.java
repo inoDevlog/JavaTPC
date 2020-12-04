cd4a7bca978cd3fdd75ba60ff987b41d43b9f183
choiinho <choiinho@knou.ac.kr>
2020-12-04 16:50:21 +0900

    git

--- src/TPC02.java
+++ src/TPC02.java
@@ -1,10 +1,11 @@
 
 public class TPC02 {
 	public static void main(String[] args) {
+		
 		// 프로그래밍의 3대 요소 : 변수, 자료형(DataType), 할당(=)
 		// 1+1=2
 
-		int a, b, c; // 4byte. 자료혀은 크기와 종류가 정해져야 한다.
+		int a, b, c; // 4byte. 자료형은 크기와 종류가 정해져야 한다.
 		a = 1;
 		b = 1;
 		c = a + b;
