4cfc9562ce0ce4ecea5f335f140e4dbaec921faa
choiinho <ino0319@gmail.com>
2020-12-20 23:47:22 +0900

    tpc07

--- src/TPC07.java
+++ src/TPC07.java
@@ -7,6 +7,18 @@ public class TPC07 {
 		// a+b=?
 		float v = sum(a, b);
 		System.out.println(v);
+
+		int[] arr = { 1, 2, 3, 4, 5 };
+		int vv = arrSum(arr);
+		System.out.println(vv);
+	}
+
+	private static int arrSum(int[] x) {
+		int hap = 0;
+		for (int i = 0; i < x.length; i++) {
+			hap += x[i];
+		}
+		return hap;
 	}
 
 	public static float sum(int a, float b) {
