0d21eb44944913456fa5987ba25c9912a9a6d9a3
choiinho <ino0319@gmail.com>
2021-01-01 00:55:19 +0900

    Overloading

---
+++ src/TPC12.java
@@ -0,0 +1,24 @@
+import kr.tpc.BookVO;
+
+public class TPC12 {
+	public static void main(String[] args) {
+		// 생성자 -> 생성+초기화 -> 중복정의
+		BookVO b1 = new BookVO();
+
+		System.out.print(b1.title + "\t");
+		System.out.print(b1.price + "\t");
+		System.out.print(b1.company + "\t");
+		System.out.println(b1.page);
+
+		BookVO b2 = new BookVO();
+
+		System.out.print(b2.title + "\t");
+		System.out.print(b2.price + "\t");
+		System.out.print(b2.company + "\t");
+		System.out.println(b2.page);
+
+		BookVO b3 = new BookVO("Python", 18000, "DAELIM", 920);
+		System.out.println(b3.title + "\t" + b3.price + "\t" + b3.company + "\t" + b3.page);
+
+	}
+}
--- src/kr/tpc/BookVO.java
+++ src/kr/tpc/BookVO.java
@@ -7,4 +7,20 @@ public class BookVO {
 	public String company;
 	public int page;
 
+	// 디폴트 생성자메서드(생략)
+	public BookVO() {
+		// init
+		this.title = "Java";
+		this.price = 14000;
+		this.company = "ESYS";
+		this.page = 780;
+	}
+
+	// 생성자 메서드의 중복정의(Overloading)
+	public BookVO(String title, int price, String company, int page) {
+		this.title = title;
+		this.price = price;
+		this.company = company;
+		this.page = page;
+	}
 }
