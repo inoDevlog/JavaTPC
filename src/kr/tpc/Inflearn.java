21f58bbbd35585469c9c73b0847367b1b596c347
choiinho <ino0319@gmail.com>
2021-01-04 23:59:17 +0900

    static

---
+++ src/kr/tpc/Inflearn.java
@@ -0,0 +1,16 @@
+package kr.tpc;
+
+public class Inflearn {
+	public Inflearn() {
+
+	}
+
+	public void tpc() {
+		System.out.println("TPC강의 너무 재미있다.");
+	}
+
+	public static void java() {
+		System.out.println("Java강의 너무 재미있다.");
+	}
+
+}
