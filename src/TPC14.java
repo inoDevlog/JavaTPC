86e43def115cf260a5f625723c21b343a908700b
choiinho <ino0319@gmail.com>
2021-01-17 23:57:19 +0900

    get / set

---
+++ src/TPC14.java
@@ -0,0 +1,9 @@
+import kr.tpc.MemberVO;
+
+public class TPC14 {
+	public static void main(String[] args) {
+
+		MemberVO m = new MemberVO();
+	}
+
+}
--- src/kr/tpc/Inflearn.java
+++ src/kr/tpc/Inflearn.java
@@ -1,7 +1,7 @@
 package kr.tpc;
 
 public class Inflearn {
-	
+
 	private Inflearn() {
 
 	}
