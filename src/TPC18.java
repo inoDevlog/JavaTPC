1e192267acc5d4ac27849b5b180394d2b659a790
choiinho <ino0319@gmail.com>
2021-03-04 22:47:34 +0900

    tpc18

---
+++ src/TPC18.java
@@ -0,0 +1,18 @@
+// import java.lang.*;
+public class TPC18 {
+
+	public static void main(String[] args) {
+
+		// 1. Java에서 제공해주는 class들.. API
+		// 문자열(String)
+		String str = new String("APPLE");
+		System.out.println(str.toLowerCase());
+		
+		// 2. 직접 만들어서 사용하는 class들...DTO/VO, DAO, Utility
+
+		// 3. 다른 회사에서 만들어 놓은 class들... API
+		
+		
+	}
+
+}
